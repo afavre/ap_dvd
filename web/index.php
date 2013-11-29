2acd65a4a8e857300e7f05bead16c10fab37d171
pierre <pierre.gost@gmail.com>
2013-11-30 00:11:56 +0100

    suppression d'un echo de test

--- web/index.php
+++ web/index.php
@@ -1,6 +1,5 @@
 <?php
 
-echo 'rrrr';
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
